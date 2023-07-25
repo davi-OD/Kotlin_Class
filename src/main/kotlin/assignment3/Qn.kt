7f2821a877f4100a2bc30eeb3c7c7168cf016258
davi-OD <davidodie51@gmail.com>
2023-07-26 00:10:23 +0300

    assignment 3

--- src/main/kotlin/assignment3/Qn.kt
+++ src/main/kotlin/assignment3/Qn.kt
@@ -1,5 +1,39 @@
 package assignment3
 
+fun main() {
+    println("Enter first number:")
+    val firstDouble = readln().toDouble()
+    println("Enter second number:")
+    val secondDouble = readln().toDouble()
+    println("Enter a character i.e. '*,+,-,/'")
+    val characterX = readln()[0]
+    actionUser(firstDouble, secondDouble, characterX)
+}
+
+fun actionUser(a:Double, b:Double, c:Char) {
+    val ansr: Double
+    when (c) {
+        '*' -> {
+            ansr = a * b
+            println("The multiplication of $a and $b is $ansr")
+        }
+        '+' -> {
+            ansr = a + b
+            println("The sum of $a and $b is $ansr")
+        }
+        '-' -> {
+            ansr = a - b
+            println("The difference between $a and $b is $ansr")
+        }
+        '/' -> {
+            ansr = a / b
+            println("The division of $a and $b is $ansr")
+        }
+        else -> {
+            println("Enter a recognized character")
+        }
+    }
+}
 /**
 //write a function that has three parameters; two doubles and one character
 //(*, +, -, /) and checks if third parameter
@@ -8,3 +42,4 @@ package assignment3
 // == -, it will subtract the two numbers
 // == /, it will divide the two numbers
  * */
+
